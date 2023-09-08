e8913d019e3694dc70eef5c042b2f1932d3dbb15
daryl <darylchua@hotmail.sg>
2023-09-08 14:07:46 +0800

    Create init.sql
    for localDB testing

---
+++ init.sql
@@ -0,0 +1,5 @@
+create database TEST;
+use TEST;
+create table user(id int, data varchar(255));
+insert into user(id, data) values (1, 'john smith');
+insert into user(id, data) values (2, 'jada smith');
