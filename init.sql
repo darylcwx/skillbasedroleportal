d7c0b9dd537006b1425a067e3476bbeb93041271
Wen Hai <91234943+wenhai-smu@users.noreply.github.com>
2023-09-16 16:02:52 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,33 +1,13 @@
 # Client name: All-In-One (AIO)
 # Project name: Skill Based Role Portal
 
+# clear start for database
 drop database if exists SBRP_Ais_Kachang;
 create database SBRP_Ais_Kachang;
 use SBRP_Ais_Kachang;
 
-# ============== Original ==================
-CREATE TABLE IF NOT EXISTS user(
-    id INT NOT NULL AUTO_INCREMENT,
-    data VARCHAR(255),
-    PRIMARY KEY (id)
-);
-
-insert into user(id, data) VALUES (1, 'john smith');
-insert into user(id, data) VALUES (2, 'jada smith');
-
-CREATE TABLE IF NOT EXISTS role(
-    id int NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255), 
-    description VARCHAR(255),
-    PRIMARY KEY (id)
-);
-
-INSERT INTO role(name, description) VALUES ('Staff', 'Any staff applying for roles');
-INSERT INTO role(name, description) VALUES ('Human Resources', 'Any member of the Human Resources team');
-INSERT INTO role(name, description) VALUES ('Manager', 'Any manager');
-# ================== End of Original ================
 # ================== Created based on Project Specification ================== 
-# Can create more properties as needed from the project.
+# Can create more properties as needed for the project.
 # To add more dummy data for testing purposes.
 
 create table `Staff` (
@@ -40,14 +20,24 @@ create table `Staff` (
 	constraint Staff_PK primary key (`Staff_ID`)
 );
 
-insert into `Staff` values(1, 'King', 'Kong', 'HR', 'king.kong@aio.com', 1) ;
-insert into `Staff` values(1, 'John', 'Smith', 'IT', 'john.smith@aio.com', 1) ;
+insert into `Staff` values(1, 'Jude', 'Smith', 'IT', 'jude.smith@aio.com', 0) ;
+insert into `Staff` values(2, 'King', 'Kong', 'HR', 'king.kong@aio.com', 1) ;
+insert into `Staff` values(3, 'John', 'Smith', 'IT', 'john.smith@aio.com', 1) ;
+insert into `Staff` values(4, 'Little', 'Tip', 'Finance', 'little.tip@aio.com', 2) ;
+insert into `Staff` values(5, 'Chicken', 'Ninja', 'HR', 'chicken.ninja@aio.com', 2) ;
+insert into `Staff` values(6, 'staff', '01', 'IT', 'staff.01@aio.com', 1) ;
 
 create table `Role` (
 	`Role_Name` varchar(20) NOT NULL,
 	`Role_Desc` longtext NOT NULL,
+    `Deadline` date NOT NULL,
 	constraint Role_PK primary key (`Role_Name`)
 );
+
+INSERT INTO `Role` VALUES ('IT Support', 'Assit in daily user technical problems and IT equipment maintenence.', '2023-10-25');
+INSERT INTO `Role` VALUES ('Clerk', 'Keyboard warrior.', '2023-10-28');
+INSERT INTO `Role` VALUES ('Human Resources', 'Any member of the Human Resources team', '2023-11-18');
+INSERT INTO `Role` VALUES ('Manager', 'Any manager', '2023-11-05');
 
 create table `Role_Skill` (
 	`Role_Name` varchar(20) NOT NULL,
@@ -64,3 +54,26 @@ create table `Staff_Skill` (
 	constraint Staff_Skill_FK1 foreign key (`Staff_ID`) references `Staff` (`Staff_ID`),
 	constraint Staff_Skill_FK2 foreign key (`Skill_Name`) references `Role_Skill` (`Skill_Name`)
 );
+
+create table `Staff_Application` (
+	`Application_ID` int(10) NOT NULL AUTO_INCREMENT,
+	`Staff_ID` int(10) NOT NULL,
+    `Role_Name` varchar(20) NOT NULL,
+    constraint Staff_Application_PK primary key (`Application_ID`),
+    constraint Staff_Application_FK1 foreign key (`Staff_ID`) references `Staff` (`Staff_ID`),
+    constraint Staff_Application_FK2 foreign key (`Role_Name`) references `Role` (`Role_Name`)
+);
+
+INSERT INTO `Staff_Application` VALUES ('1', 6, 'Clerk');
+
+create table `Staff_Login` (
+    `Username` varchar(20) NOT NULL,
+    `Password` longtext NOT NULL,
+    `Staff_ID` int(10) NOT NULL,
+    constraint Staff_Login_PK primary key (`Username`),
+    constraint Staff_Login_FK1 foreign key (`Staff_ID`) references `Staff` (`Staff_ID`)
+);
+
+INSERT INTO `Staff_Login` VALUES ('staff01', 'staff01password', 6);
+INSERT INTO `Staff_Login` VALUES ('jude', 'jude00password', 1);
+INSERT INTO `Staff_Login` VALUES ('king', 'king00password', 2);
