4813f36f1717801dc0132c040bae85125d7152d0
Waisoon123 <waisoon1999@gmail.com>
2023-09-25 10:20:13 +0800

    Update Home.Vue statically.
    Static backbone, to retrieve and populate data dynamically.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,9 +5,13 @@
             <RoleListing :role=roleObject />
             <RoleListing :role=roleObject />
             <RoleListing :role=roleObject />
+            <!-- Here should use v-for to iterate through roleListings -->
+            <RoleListing v-for="roleItem in roleListings" :key="roleItem.id" :role="roleItem"/>
         </div>
     </div>
 </template>
+
+
 <script>
 import RoleListing from "../components/RoleListing.vue"
 export default {
@@ -21,6 +25,34 @@ export default {
         };
     },
     created() {
+        // Fetch Data 
+        // I'm presuming that we are using the axios that is taught to fetch the data from the database.
+        // Trying out with static data here first;
+        this.roleListings = [
+            {
+                id: 1,
+                roleName: "Software Engineer",
+                roleDesc: "We are seeking a talented and motivated Software Engineer to join our dynamic team. As a Software Engineer, you will play a pivotal role in designing, developing, and maintaining cutting-edge software solutions. You'll collaborate with cross-functional teams to translate business requirements into scalable and efficient software systems. The ideal candidate is passionate about technology, possesses strong problem-solving skills, and has a proven track record of delivering high-quality code. Join us in shaping the future of our products and contributing to our mission of delivering innovative software solutions to our customers.",
+                roleDeadline: "27/04/2023",
+                skillsRequired: ["Scrum", "Python", "Design Thinking"],
+            },
+
+            {
+                id: 1,
+                roleName: "Software Engineer",
+                roleDesc: "We are seeking a talented and motivated Software Engineer to join our dynamic team. As a Software Engineer, you will play a pivotal role in designing, developing, and maintaining cutting-edge software solutions. You'll collaborate with cross-functional teams to translate business requirements into scalable and efficient software systems. The ideal candidate is passionate about technology, possesses strong problem-solving skills, and has a proven track record of delivering high-quality code. Join us in shaping the future of our products and contributing to our mission of delivering innovative software solutions to our customers.",
+                roleDeadline: "27/04/2023",
+                skillsRequired: ["Scrum", "Python", "Design Thinking"],
+            },
+
+            {
+                id: 1,
+                roleName: "Software Engineer",
+                roleDesc: "We are seeking a talented and motivated Software Engineer to join our dynamic team. As a Software Engineer, you will play a pivotal role in designing, developing, and maintaining cutting-edge software solutions. You'll collaborate with cross-functional teams to translate business requirements into scalable and efficient software systems. The ideal candidate is passionate about technology, possesses strong problem-solving skills, and has a proven track record of delivering high-quality code. Join us in shaping the future of our products and contributing to our mission of delivering innovative software solutions to our customers.",
+                roleDeadline: "27/04/2023",
+                skillsRequired: ["Scrum", "Python", "Design Thinking"],
+            },
+        ];
 
     },
     mounted() {
