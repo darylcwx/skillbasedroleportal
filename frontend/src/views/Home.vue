54318b77b39ca9204ab0394eca4980bd0f5f7a35
Waisoon123 <waisoon1999@gmail.com>
2023-09-25 12:57:24 +0800

    Update Home.vue to dynamically retrieve date in a revised format
    Previously include 00:00:00:00 GMT, intended string format should be "Day, Date, Month, Year"

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,9 +2,6 @@
     <div class="container mx-auto max-w-4xl h-screen mt-12">
         <!-- 0: admin, 1: user, 2: manager -->
         <div class="h-auto pt-4">
-            <!-- <RoleListing :role=roleObject />
-            <RoleListing :role=roleObject />
-            <RoleListing :role=roleObject /> -->
             <!-- Here should use v-for to iterate through roleListings -->
             <RoleListing v-for="roleItem in roleListings" :key="roleItem.id" :role="roleItem"/>
         </div>
@@ -25,35 +22,7 @@ export default {
         };
     },
     created() {
-        // Fetch Data 
-        // I'm presuming that we are using the axios that is taught to fetch the data from the database.
-        // Trying out with static data here first;
-        this.roleListings = [
-            {
-                id: 1,
-                roleName: "Software Engineer",
-                roleDesc: "We are seeking a talented and motivated Software Engineer to join our dynamic team. As a Software Engineer, you will play a pivotal role in designing, developing, and maintaining cutting-edge software solutions. You'll collaborate with cross-functional teams to translate business requirements into scalable and efficient software systems. The ideal candidate is passionate about technology, possesses strong problem-solving skills, and has a proven track record of delivering high-quality code. Join us in shaping the future of our products and contributing to our mission of delivering innovative software solutions to our customers.",
-                roleDeadline: "27/04/2023",
-                skillsRequired: ["Scrum", "Python", "Design Thinking"],
-            },
-
-            {
-                id: 1,
-                roleName: "Software Engineer",
-                roleDesc: "We are seeking a talented and motivated Software Engineer to join our dynamic team. As a Software Engineer, you will play a pivotal role in designing, developing, and maintaining cutting-edge software solutions. You'll collaborate with cross-functional teams to translate business requirements into scalable and efficient software systems. The ideal candidate is passionate about technology, possesses strong problem-solving skills, and has a proven track record of delivering high-quality code. Join us in shaping the future of our products and contributing to our mission of delivering innovative software solutions to our customers.",
-                roleDeadline: "27/04/2023",
-                skillsRequired: ["Scrum", "Python", "Design Thinking"],
-            },
-
-            {
-                id: 1,
-                roleName: "Software Engineer",
-                roleDesc: "We are seeking a talented and motivated Software Engineer to join our dynamic team. As a Software Engineer, you will play a pivotal role in designing, developing, and maintaining cutting-edge software solutions. You'll collaborate with cross-functional teams to translate business requirements into scalable and efficient software systems. The ideal candidate is passionate about technology, possesses strong problem-solving skills, and has a proven track record of delivering high-quality code. Join us in shaping the future of our products and contributing to our mission of delivering innovative software solutions to our customers.",
-                roleDeadline: "27/04/2023",
-                skillsRequired: ["Scrum", "Python", "Design Thinking"],
-            },
-        ];
-
+        // Remove static data from previous version;
     },
     mounted() {
         // get/RoleListings
@@ -68,6 +37,12 @@ export default {
                 const apiUrl = "http://localhost:5000/api/rolelistings";
                 const response = await fetch(apiUrl, { mode: "cors" });
                 const data = await response.json();
+
+                // Format the date in each role item
+                data.Listings.forEach((roleItem) => {
+                    roleItem.Deadline = new Date(roleItem.Deadline).toDateString();
+                });
+
                 this.roleListings = data.Listings;
                 console.log(this.roleListings);
             } catch (error) {
