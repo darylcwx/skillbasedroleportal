50d2b64a42eed9d36b0fc7d51fa937e6f5498383
Xavier Low <95678174+xavierlowjunjie@users.noreply.github.com>
2023-09-29 13:19:21 +0800

    dynamically update filter based on staff skills

--- frontend/src/components/SearchFilter.vue
+++ frontend/src/components/SearchFilter.vue
@@ -1,59 +1,93 @@
 <template>
-    <div class="d-flex flex-row align-items-stretch mt-4">
-        <div class="flex-grow-1 mr-3">
-            <div class="input-group rounded">
-                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-            </div>
-        </div>
-        
-        <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle"
-                    type="button"
-                    id="multiSelectDropdown"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                <!-- {{ dropdownButtonText }} -->
-                Filter
-            </button>
-            <ul class="dropdown-menu"
-                aria-labelledby="multiSelectDropdown">
-                <li v-for="(item, index) in items" :key="index">
-                <label class="ml-2">
-                    <!-- <input type="checkbox" :value="item.value" v-model="item.selected" @change="handleCheckboxChange" /> -->
-                    <input type="checkbox" :value="item.value" v-model="item.selected" />
-                    {{ item.label }}
-                </label>
-                </li>
-            </ul>
-        </div>
+  <div class="d-flex flex-row align-items-stretch mt-4">
+    <div class="flex-grow-1 mr-3">
+      <div class="input-group rounded">
+        <input
+          type="search"
+          class="form-control rounded"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="search-addon"
+        />
+      </div>
     </div>
 
+    <div class="dropdown">
+      <button
+        class="btn btn-primary dropdown-toggle"
+        type="button"
+        id="multiSelectDropdown"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <!-- {{ dropdownButtonText }} -->
+        Filter
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="multiSelectDropdown">
+        <li v-for="(item, index) in items" :key="index">
+          <label class="ml-2">
+            <!-- <input type="checkbox" :value="item.value" v-model="item.selected" @change="handleCheckboxChange" /> -->
+            <input
+              type="checkbox"
+              :value="item"
+              v-model="selectedItems"
+              @change="deletelater()"
+            />
+            {{ item }}
+          </label>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-data() {
+  data() {
     return {
-    items: [
-        { label: "Java", value: "Java", selected: false },
-        { label: "C++", value: "C++", selected: false },
-        { label: "Python", value: "Python", selected: false },
-    ],
+      user: this.$store.state.user,
+      showDevPanel: false,
+      selectedItems: [],
+      items: [],
+      //   items: [
+      //     { label: 'Java', value: 'Java', selected: false },
+      //     { label: 'C++', value: 'C++', selected: false },
+      //     { label: 'Python', value: 'Python', selected: false },
+      //   ],
     };
-},
-computed: {
+  },
+  computed: {
     dropdownButtonText() {
-    const selectedItems = this.items.filter((item) => item.selected).map((item) => item.label);
-    return selectedItems.length > 0 ? selectedItems.join(', ') : 'Select Items';
+      const selectedItems = this.items
+        .filter((item) => item.selected)
+        .map((item) => item.label);
+      return selectedItems.length > 0
+        ? selectedItems.join(', ')
+        : 'Select Items';
     },
-},
-methods: {
-},
-mounted() {
+  },
+  methods: {
+    async getStaffSkillsbyID() {
+      try {
+        const apiUrl =
+          'http://localhost:5000/api/staffskills?sid=' + this.user.id;
+        const response = await fetch(apiUrl, { mode: 'cors' });
+        const data = await response.json();
+        console.log(data['Staff Skills']);
+        this.items = data['Staff Skills'];
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
+    },
+    deletelater() {
+      console.log(this.selectedItems);
+    },
+  },
+  mounted() {
     const dropdownButton = document.getElementById('multiSelectDropdown');
     const dropdownMenu = document.querySelector('.dropdown-menu');
-
     dropdownMenu.addEventListener('change', this.handleCheckboxChange);
-},
+    this.getStaffSkillsbyID();
+  },
 };
 </script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,64 +1,74 @@
 <template>
-    <div class="container mx-auto max-w-2xl min-h-[calc(100vh-56px)] mt-14">
-        <div class="mt-4">
-            <SearchFilter></SearchFilter>
-        </div>
-        <div class="">
-            <RoleListingPanel v-for="roleItem in roleListings" :key="roleItem.id" :role="roleItem" />
-        </div>
-        <div v-if="roleListingsIsEmpty" class="h-[calc(100vh-56px)] grid place-content-center text-white">
-            <div class="flex flex-col">
-                <ExclamationTriangleIcon class="h-12 w-12 text-red-500 mx-auto" />
-                <div>Oops! There are
-                    currently no listings available. <br>
-                    Please check with HR or try again later.
-                </div>
-            </div>
-        </div>
+  <div class="container mx-auto max-w-2xl min-h-[calc(100vh-56px)] mt-14">
+    <div class="mt-4">
+      <SearchFilter></SearchFilter>
     </div>
+    <div class="">
+      <RoleListingPanel
+        v-for="roleItem in roleListings"
+        :key="roleItem.id"
+        :role="roleItem"
+      />
+    </div>
+    <div
+      v-if="roleListingsIsEmpty"
+      class="h-[calc(100vh-56px)] grid place-content-center text-white"
+    >
+      <div class="flex flex-col">
+        <ExclamationTriangleIcon class="h-12 w-12 text-red-500 mx-auto" />
+        <div>
+          Oops! There are currently no listings available. <br />
+          Please check with HR or try again later.
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import RoleListingPanel from "../components/RoleListingPanel.vue";
-import SearchFilter from "../components/SearchFilter.vue";
-import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline";
+import RoleListingPanel from '../components/RoleListingPanel.vue';
+import SearchFilter from '../components/SearchFilter.vue';
+import { ExclamationTriangleIcon } from '@heroicons/vue/24/outline';
 export default {
-    components: { RoleListingPanel, ExclamationTriangleIcon, SearchFilter },
-    data() {
-        return {
-            user: this.$store.state.user,
-            roleListings: [],
-            roleListingsIsEmpty: false,
-            // listing_id, Role_Name, Description, Deadline
-        };
-    },
-    created() {
-        // Remove static data from previous version;
-    },
-    mounted() {
-        // get/RoleListings
-        // this.roleListings = response.data.roleListings
+  components: { RoleListingPanel, ExclamationTriangleIcon, SearchFilter },
+  data() {
+    return {
+      user: this.$store.state.user,
+      roleListings: [],
+      roleListingsIsEmpty: false,
+      // listing_id, Role_Name, Description, Deadline
+    };
+  },
+  created() {
+    // Remove static data from previous version;
+  },
+  mounted() {
+    // get/RoleListings
+    // this.roleListings = response.data.roleListings
 
-        // console.log(this.user);
-        this.fetchRoleListings();
+    // console.log(this.user);
+    this.fetchRoleListings();
+  },
+  methods: {
+    async fetchRoleListings() {
+      try {
+        const apiUrl = 'http://localhost:5000/api/rolelistings';
+        const response = await fetch(apiUrl, { mode: 'cors' });
+        const data = await response.json();
+        // Format the date in each role item
+        data.Listings.forEach((roleItem) => {
+          const options = { year: 'numeric', month: 'long', day: 'numeric' };
+          roleItem.Deadline = new Date(roleItem.Deadline).toLocaleString(
+            'en-US',
+            options
+          );
+        });
+        this.roleListings = data.Listings;
+      } catch (error) {
+        this.roleListingsIsEmpty = true;
+        console.error(error);
+      }
     },
-    methods: {
-        async fetchRoleListings() {
-            try {
-                const apiUrl = "http://localhost:5000/api/rolelistings";
-                const response = await fetch(apiUrl, { mode: "cors" });
-                const data = await response.json();
-                // Format the date in each role item
-                data.Listings.forEach((roleItem) => {
-                    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-                    roleItem.Deadline = new Date(roleItem.Deadline).toLocaleString("en-US", options);
-                });
-                this.roleListings = data.Listings;
-            } catch (error) {
-                this.roleListingsIsEmpty = true;
-                console.error(error);
-            }
-        },
-    },
+  },
 };
 </script>
