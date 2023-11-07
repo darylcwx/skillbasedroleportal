823c144c71e15e9f06b5da6b4904f78f92442f73
daryl <darylchua@hotmail.sg>
2023-11-07 14:56:36 +0800

    Update conftest.py

--- selenium_tests/conftest.py
+++ selenium_tests/conftest.py
@@ -6,14 +6,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture()
 def driver():
-    try: 
         print("Creating chrome driver")
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        # options = webdriver.ChromeOptions()
+        # options.add_argument("--headless")
+        #my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         yield my_driver
         print("Closing chrome driver")
         my_driver.quit()
-    except Exception as e:
-        print(e)
     
