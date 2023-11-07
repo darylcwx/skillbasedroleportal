2bd8f456ba2bd42e94e992fe64d77ef55c65ee6b
daryl <darylchua@hotmail.sg>
2023-11-07 13:12:15 +0800

    Update conftest.py

--- selenium_tests/conftest.py
+++ selenium_tests/conftest.py
@@ -6,8 +6,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture()
 def driver():
-    print("Creating chrome driver")
-    my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    yield my_driver
-    print("Closing chrome driver")
+    try: 
+        print("Creating chrome driver")
+        my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        yield my_driver
+        print("Closing chrome driver")
+    except Exception as e:
+        print(e)
     my_driver.quit()
